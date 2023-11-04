344c0c39ca7b2c91636af0f145aa866bd1baf78b
alejandroOleaL <alejandroolea58@gmail.com>
2023-11-04 15:44:19 -0600

    Se agrega url en resource

--- src/main/java/app/netlify/leones/gym/back/auth/ResourceServerConfig.java
+++ src/main/java/app/netlify/leones/gym/back/auth/ResourceServerConfig.java
@@ -41,7 +41,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*"));
+		config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "*", "https://leonesgym.web.app"));
 		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "Access-Control-Allow-Origin"));
