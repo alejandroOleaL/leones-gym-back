aa1f89b39e630655dd79fdc17ac1ce73e09511b8
alejandroOleaL <alejandroolea58@gmail.com>
2023-11-04 19:20:01 -0600

    Se agregan cambios en el metodo principal

--- src/main/java/app/netlify/leones/gym/back/LeonesGymBackApplication.java
+++ src/main/java/app/netlify/leones/gym/back/LeonesGymBackApplication.java
@@ -15,13 +15,16 @@ public class LeonesGymBackApplication {
 	}
 	
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("**");
-			}
-		};
-	}
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+            }
+        };
+    }
 	
 }
