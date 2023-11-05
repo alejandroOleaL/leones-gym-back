88cefe864c5bc3b0d882c9637776e3cdb396fb3c
alejandroOleaL <alejandroolea58@gmail.com>
2023-11-04 18:15:35 -0600

    Se agrega configuracion en la clase principal

--- src/main/java/app/netlify/leones/gym/back/LeonesGymBackApplication.java
+++ src/main/java/app/netlify/leones/gym/back/LeonesGymBackApplication.java
@@ -2,6 +2,10 @@ package app.netlify.leones.gym.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LeonesGymBackApplication {
@@ -9,5 +13,15 @@ public class LeonesGymBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LeonesGymBackApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+	
 }
