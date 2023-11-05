89a7ff66ca9382dc67d1fe02ee45d50733d91681
alejandroOleaL <alejandroolea58@gmail.com>
2023-11-04 18:32:30 -0600

    Se agrega doble asteriscos

--- src/main/java/app/netlify/leones/gym/back/LeonesGymBackApplication.java
+++ src/main/java/app/netlify/leones/gym/back/LeonesGymBackApplication.java
@@ -19,7 +19,7 @@ public class LeonesGymBackApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/**").allowedOrigins("**");
 			}
 		};
 	}
