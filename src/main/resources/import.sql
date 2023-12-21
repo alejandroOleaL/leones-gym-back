19158d51eaec2f8d53e28fb86b4be7ddc377c6e3
alejandroOleaL <alejandroolea58@gmail.com>
2023-12-20 19:28:32 -0600

    Se agregan mas clientes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,9 +5,15 @@ INSERT INTO periodos (id, periodo, nombre) VALUES(4, 90, 'Trimestre');
 INSERT INTO periodos (id, periodo, nombre) VALUES(5, 180, 'Semestre');
 INSERT INTO periodos (id, periodo, nombre) VALUES(6, 365, 'Anual');
 
-INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(1, 'alex', 'locia', '2023-11-08', '7471', 'alex@mail.com', '', 1203, 7, false);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(1, 'alex', 'locia', '2023-12-26', '7471', 'alex@mail.com', '', 1203, 7, true);
 INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(2, 'bryan', 'lopez', '2023-09-22', '837636', 'bryan@mail.com', '', 1111, 15, false);
-INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(3, 'wilianismo', '',  '2023-03-12', '64747', 'wiliam@mail.com', '', 8621, 30, false);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(3, 'wilianismo', 'cri',  '2023-03-12', '64747', 'wiliam@mail.com', '', 8621, 30, false);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(4, 'andres', 'gordo', '2023-12-29', '7471', 'alex@mail.com', '', 1203, 7, true);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(5, 'panchogro', 'lopez', '2023-09-22', '837636', 'bryan@mail.com', '', 1111, 15, false);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(6, 'erika', 'salmeda',  '2023-03-12', '64747', 'wiliam@mail.com', '', 8621, 30, false);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(7, 'samehada', 'val', '2023-12-27', '7471', 'alex@mail.com', '', 1203, 7, true);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(8, 'juan', 'el pana', '2023-09-22', '837636', 'bryan@mail.com', '', 1111, 15, false);
+INSERT INTO clientes (periodo_id, nombre, apellidos, fecha_fin, telefono, correo, foto, num_control, dias_periodo, estatus) VALUES(9, 'atz', 'alv',  '2023-03-12', '64747', 'wiliam@mail.com', '', 8621, 30, false);
 
 INSERT INTO usuarios (nombre, apellidos, email, username, password, enabled) VALUES('supremo', 'kaiosama', 'sokaio@mail.com', 'user', '$2a$10$lm8H0.J3pi1FpM3s3CUpe.Ym0FnlwHFclzGIGoWItLCN1vIFWEMmm', 1);
 INSERT INTO usuarios (nombre, apellidos, email, username, password, enabled) VALUES('andres', 'gordo', 'angor@mail.com', 'admin', '$2a$10$g2//5daw/XMhgjPbkryr7OL5h05zauUHrNhBL8ey6V0NNQHV/3dGm', 1);
