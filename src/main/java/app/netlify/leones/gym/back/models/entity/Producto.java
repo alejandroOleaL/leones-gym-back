d50f197021640220c3ba30fb2d67c776f55b4386
alejandroOleaL <alejandroolea58@gmail.com>
2024-02-26 23:36:01 -0600

    Se sube clase de producto

---
+++ src/main/java/app/netlify/leones/gym/back/models/entity/Producto.java
@@ -0,0 +1,96 @@
+package app.netlify.leones.gym.back.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private Double precio;
+	private Integer stock;
+	private String descripcion;
+	
+	@Column(name = "fecha_registro")
+	@Temporal(TemporalType.DATE)
+	private Date fechaRegistro;
+	
+	@PrePersist
+	public void prePersist() {
+		this.fechaRegistro = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public Integer getStock() {
+		return stock;
+	}
+
+	public void setStock(Integer stock) {
+		this.stock = stock;
+	}
+
+	public Date getFechaRegistro() {
+		return fechaRegistro;
+	}
+
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", stock=" + stock
+				+ ", descripcion=" + descripcion + ", fechaRegistro=" + fechaRegistro + "]";
+	}
+
+}
